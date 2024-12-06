337b51e6633cb95fedf588ff00fe12feeeb94190
ShubhamKumar <shubhamk@saxon.ai>
2024-12-06 15:16:53 +0530

    migration and update databse done

--- Application.Services/Repositories/DBContext/ApplicationDBContext.cs
+++ Application.Services/Repositories/DBContext/ApplicationDBContext.cs
@@ -1,12 +1,16 @@
 ﻿using Application.Services.Models.Entities;
 using Microsoft.EntityFrameworkCore;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
 
 namespace Application.Services.Repositories.DBContext
 {
     public class ApplicationDBContext : DbContext
     {
-        DbSet<Project> projects;
-        DbSet<Department> department;
-        DbSet<Employee> employees;
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
+
+
+        DbSet<Project> Projects { get; set; }
+        DbSet<Department> Departments { get; set; }
+        DbSet<Employee> Employees { get; set; }
     }
 }
