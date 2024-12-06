6e39a7d16d79639b6026fcac7ac15bb7577d913e
Somesh Banjare <sbanjare@saxon.ai>
2024-12-06 14:53:10 +0530

    Added Entities

--- Application.Services/Models/Entities/Employee.cs
+++ Application.Services/Models/Entities/Employee.cs
@@ -2,5 +2,23 @@
 {
     public class Employee
     {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string LastName { get; set; }
+        //  public string Email { get; set; }
+        public string? YearOfExperience { get; set; }
+        public string PhoneNO { get; set; }
+        public string? Aadhar { get; set; }
+        public string? PAN { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmployeeGender { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public double? AnnualPackage { get; set; }
+        public double? MonthlySalary { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        public Department Department { get; set; }
+        public ICollection<Project> Projects { get; set; }
     }
 }
---
+++ Application.Services/Models/RequestDTO/EmployeeRequestDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Models.RequestDTO
+{
+    public class EmployeeRequestDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNO { get; set; }
+        // public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string EmployeeGender { get; set; }
+        public DateTime DateOfJoining { get; set; }
+        public string Aadhar { get; set; }
+    }
+}
